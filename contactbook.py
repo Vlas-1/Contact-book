11c77acdb411abcb8af5fa39adf3fd340bc96804
Vlas-1 <72249823+Vlas-1@users.noreply.github.com>
2021-02-27 10:32:27 +0300

    Add files via upload

--- contactbook.py
+++ contactbook.py
@@ -1,8 +1,11 @@
 import sys
 import sqlite3
 import argparse
-contact_name='' #Имя контакта
-contact_num='' #Номер контакта
+contact_name=''
+contact_num=''
+Contacts=['Ваши контакты:']
+colContacts=0
+del_contact=0
 conn = sqlite3.connect('orderese.db')
 cur = conn.cursor()
 cur.execute("""CREATE TABLE IF NOT EXISTS contacts(
@@ -11,44 +14,56 @@ cur.execute("""CREATE TABLE IF NOT EXISTS contacts(
 """)
 conn.commit()
 
-def add_contact(contact_num,contact_name): #Добавление контакта
+def add_contact(contact_num,contact_name,Contacts,colContacts): #Добавление контакта
     contact_name=input("Введите имя контакта:")
     contact_num=input("Ведите номер контакта:")
     cur.execute('INSERT INTO contacts(name, number) VALUES(?,?)', (contact_name, contact_num))
     conn.commit()
-    remove1()
-def delete_contact(): #Удаление контакта
-    del_contact=input("Имя контакта которого нужно удалить:")
+    remove1(del_contact)
+def delete_contact(del_contact,Contacts): #Удаление контакта
+    del_contact=input("Имя контакта которого нужно удалить. Для отмены введите-5:")
     cur.execute('DELETE FROM contacts WHERE name=?', [del_contact])
     conn.commit()
-    remove1()
+    remove1(del_contact)
 def cloud_save(): #Просмотр сохраннёного контакта
     cur.execute("SELECT * FROM contacts;")
     one_result = cur.fetchone()
     print(one_result)
-    remove1()
+    remove1(del_contact)
 def remove(): #Начальный ввод
-    can=int(input("Что делать? Добавить контакт-0, Удалить контакт-1, Показать все контакты-2:"))
+    can=int(input("Что делать? Добавить контакт-0, Удалить контакт-1, Показать сохранёный контакт-2, Выход-3:"))
     if can==0:
-        add_contact(contact_num,contact_name)
+        add_contact(contact_num,contact_name,Contacts,colContacts)
     if can==1:
-        delete_contact()
+        delete_contact(del_contact,Contacts)
     if can==2:
         cloud_save()
-def remove1(): #Постоянный ввод
-    can1=int(input("Введите цифру:"))
+    if can==3:
+        sys.exit()
+def remove1(del_contact): #Постоянный ввод
+    can1=int(input("Что делать? Добавить контакт-0, Удалить контакт-1, Показать сохранёный контакт-2, Выход-3:"))
     if can1==0:
-        add_contact(contact_num,contact_name)
+        add_contact(contact_num,contact_name,Contacts,colContacts)
     if can1==1:
-        delete_contact()
+        delete_contact(del_contact,Contacts)
     if can1==2:
         cloud_save()
+    if can1==3:
+        sys.exit()
 def parsing(): #Парсинг
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--add', action='store_true', help='add contact')
-    parser.add_argument('-d', '--delete', action='store_true', help='delete contact')
-    parser.add_argument('-c', '--cloud', action='store_true', help='all contacts')
+    parser.add_argument('-0', '--add', action='store_true', help='add contact')
+    parser.add_argument('-1', '--delete', action='store_true', help='delete contact')
+    parser.add_argument('-2', '--cloud', action='store_true', help='save contact')
+    parser.add_argument('-3', '--exit', action='store_true', help='exit')
     args = parser.parse_args()
-    parser.print_help()    
+    parser.print_help()
+    if args.add:
+        add_contact()
+    if args.delete:
+        delete_contact()
+    if args.cloud:
+        cloud_save()
 parsing()
 remove()
+
